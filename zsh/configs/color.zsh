cbf7cada94699391f272784851ae43a6befe638d
Jakub Ostrzołek <jakub.ostrzolek.stud@pw.edu.pl>
2022-10-06 12:55:32 +0200

    adapt autosuggestion color for zsh for truecolor

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -6,7 +6,7 @@ colors
 export CLICOLOR=1
 
 # change autosuggestion color to fit with solarized
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=10"
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#5a6f72"
 
 # set dircolors
 eval $(dircolors ~/.dircolors-solarized/dircolors.256dark)
