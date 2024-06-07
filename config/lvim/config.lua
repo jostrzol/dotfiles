9e68438155c033070e2da0c1cd87aa99c7c2c481
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-07 19:59:17 +0200

    lvim: add command :Map, add maps ]g, [g

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -16,6 +16,36 @@ lvim.plugins = {
   { "qpkorr/vim-bufkill" },
 }
 
+function _G.put(...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, '\n'))
+  return ...
+end
+
+vim.api.nvim_create_user_command(
+  "Map",
+  function()
+    local map = vim.api.nvim_exec2("silent verbose map", {output=true})
+    local buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_win_set_buf(0, buf)
+    vim.api.nvim_buf_set_name(buf, "Keyboard map")
+    local lines = {}
+    for s in map.output:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+
+    vim.api.nvim_put(lines, "", false, false)
+  end,
+  {}
+)
+
 lvim.keys.normal_mode["]c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('next', {navigation_message = false})<CR>"
 lvim.keys.normal_mode["[c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('prev', {navigation_message = false})<CR>"
+lvim.keys.normal_mode["]g"] = "<Cmd>lua vim.diagnostic.goto_next()<CR>"
+lvim.keys.normal_mode["[g"] = "<Cmd>lua vim.diagnostic.goto_prev()<CR>"
 lvim.keys.normal_mode["<Leader><Leader>"] = "<C-6>"
