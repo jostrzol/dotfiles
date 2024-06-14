e75e42153d05dc7fcdf1b663a736b74cf2c0744e
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-15 00:32:05 +0200

    lvim: config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -6,15 +6,32 @@
 
 lvim.plugins = {
   { "christoomey/vim-tmux-navigator" },
-  -- {
-  --   "edkolev/tmuxline.vim",
-  --   config = function ()
-  --   end
-  -- },
   { "mg979/vim-visual-multi" },
   { "tpope/vim-surround" },
   { "qpkorr/vim-bufkill" },
 }
+
+local null_ls = require("null-ls")
+null_ls.setup({
+  debug = true,
+  sources = {
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.formatting.markdownlint,
+    null_ls.builtins.formatting.mdformat,
+  }
+})
+
+lvim.keys.normal_mode["]c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('next', {navigation_message = false})<CR>"
+lvim.keys.normal_mode["[c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('prev', {navigation_message = false})<CR>"
+lvim.keys.normal_mode["]g"] = "<Cmd>lua vim.diagnostic.goto_next()<CR>"
+lvim.keys.normal_mode["[g"] = "<Cmd>lua vim.diagnostic.goto_prev()<CR>"
+lvim.keys.normal_mode["<M-f>"] = "<Cmd>lua require('lvim.lsp.utils').format()<CR>"
+lvim.keys.normal_mode["<Leader><Leader>"] = "<C-6>"
+
+
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.lsp.set_log_level('debug')
 
 function _G.put(...)
   local objects = {}
@@ -30,24 +47,16 @@ end
 vim.api.nvim_create_user_command(
   "Map",
   function()
-    local map = vim.api.nvim_exec2("silent verbose map", {output=true})
+    local map = vim.api.nvim_exec2("silent verbose map", { output = true })
     local buf = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_win_set_buf(0, buf)
     vim.api.nvim_buf_set_name(buf, "Keyboard map")
     local lines = {}
     for s in map.output:gmatch("[^\r\n]+") do
-        table.insert(lines, s)
+      table.insert(lines, s)
     end
 
     vim.api.nvim_put(lines, "", false, false)
   end,
   {}
 )
-
-lvim.keys.normal_mode["]c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('next', {navigation_message = false})<CR>"
-lvim.keys.normal_mode["[c"] = "<Cmd>lua require 'gitsigns'.nav_hunk('prev', {navigation_message = false})<CR>"
-lvim.keys.normal_mode["]g"] = "<Cmd>lua vim.diagnostic.goto_next()<CR>"
-lvim.keys.normal_mode["[g"] = "<Cmd>lua vim.diagnostic.goto_prev()<CR>"
-lvim.keys.normal_mode["<Leader><Leader>"] = "<C-6>"
-
-vim.wo.wrap = true
