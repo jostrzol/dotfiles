916c0023fb1ae69b705fa8466c292bbb1ee1f91c
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-29 14:52:52 +0200

    fish: add ctrl+backspace and ctrl+delete bindings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+bind \e\[3\;5~ kill-word
+bind \b backward-kill-word
+
 starship init fish | source
 pyenv init - | source
 
