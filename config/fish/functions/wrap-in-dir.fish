796968843c076df72dd46e763e80fd362b151c48
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-08-30 17:02:10 +0200

    fish: wrap-in-dir include hidden files

--- config/fish/functions/wrap-in-dir.fish
+++ config/fish/functions/wrap-in-dir.fish
@@ -2,6 +2,6 @@ function wrap-in-dir -d 'wrap the current directory in a folder of the given nam
   argparse --min-args 1 --max-args 1 -- $argv
   set -f dir "$argv[1]"
   mkdir "$dir"
-  fd --exclude "$dir" --exec-batch mv '{}' "$dir"
+  fd --max-depth 1 --hidden --exclude "$dir" --exec-batch mv '{}' "$dir"
 end
 
