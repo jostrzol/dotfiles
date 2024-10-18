35a999c975ed0edd8e3bb292e23f4842ca4e93a1
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-10-18 10:21:49 +0200

    lvim: fix runtimepath for spellfile.vim plugin

--- config/lvim/lua/autoconf/general.lua
+++ config/lvim/lua/autoconf/general.lua
@@ -3,4 +3,8 @@ vim.wo.linebreak = true
 vim.opt.clipboard = "unnamed"
 vim.opt.spelllang = "en,pl"
 
+-- fix nvim spellfile.vim plugin, which creates files under
+-- XDG_DATE_HOME/site, but looks for it under XDG_DATA_HOME :/
+vim.cmd(":set runtimepath+=" .. vim.fn.stdpath("data") .. "/site")
+
 lvim.builtin.project.manual_mode = true -- don't cd randomly on file open
