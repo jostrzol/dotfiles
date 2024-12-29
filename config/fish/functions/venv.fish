b035d312e0600f29fe70d9a7da7190473692b143
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-12-29 16:21:23 +0100

    fish: update venv

--- config/fish/functions/venv.fish
+++ config/fish/functions/venv.fish
@@ -1,9 +1,21 @@
 function venv
   set -f root (git rev-parse --show-toplevel 2>/dev/null)
   if test "$status" -ne "0"; set root .; end
-  set -f venv_dir "$root/.venv"
-  if not test -d "$venv_dir"
-    python3 -m venv "$venv_dir"
+
+  set -f venv_dir_default "$root/.venv"
+  set -f venv_dirs "$venv_dir_default" "$root/venv"
+  set -fe venv_dir_found
+
+  for venv_dir in $venv_dirs
+    if test -d "$venv_dir"
+      set -f venv_dir_found "$venv_dir"
+    end
   end
-  . "$venv_dir/bin/activate.fish"
+
+  if test -z "$venv_dir_found"
+    python3 -m venv "$venv_dir_default"
+    set -f venv_dir_found "$venv_dir_default"
+  end
+
+  . "$venv_dir_found/bin/activate.fish"
 end
