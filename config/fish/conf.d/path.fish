5f4f1c7f2a5b5a2380e62007958bdc9ad00eecb5
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-13 20:27:49 +0100

    fish: add mason bin to path

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,4 +1,5 @@
-set -a PATH "$HOME/.local/bin/"
+set -a PATH "/usr/lib/rustup/bin/"
+set -a PATH "$HOME/.local/share/lvim/mason/bin"
 set -a PATH "$HOME/.cargo/bin/"
 set -a PATH "$HOME/go/bin/"
-set -a PATH "/usr/lib/rustup/bin/"
+set -a PATH "$HOME/.local/bin/"
