1c0fde87872357864651549976c7b52ba4e70cec
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-03-06 09:10:30 +0100

    nvim: add force reveal for NeoTree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,19 @@
 ---@type LazyPluginSpec
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>r"] = { "<cmd>Neotree reveal_force_cwd<cr>", desc = "Reveal file" },
+          },
+        },
+      },
+    },
+  },
   opts = {
     filesystem = {
       filtered_items = {
