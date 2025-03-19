b92898bfa6ac2fe7b9aa2b9f30f57cfc8bbf7f52
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-03-19 19:24:54 +0100

    nvim: neo-tree add find files/words under node mappings

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,5 @@
+local telescope = require "telescope.builtin"
+
 ---@type LazyPluginSpec
 return {
   "nvim-neo-tree/neo-tree.nvim",
@@ -18,6 +20,24 @@ return {
     filesystem = {
       filtered_items = {
         visible = true,
+      },
+      window = {
+        mappings = {
+          ["<Leader>fw"] = {
+            function(state)
+              local node = state.tree:get_node()
+              telescope.live_grep { search_dirs = { node.path } }
+            end,
+            desc = "Find words under node",
+          },
+          ["<Leader>ff"] = {
+            function(state)
+              local node = state.tree:get_node()
+              telescope.find_files { search_dirs = { node.path } }
+            end,
+            desc = "Find files under node",
+          },
+        },
       },
     },
   },
