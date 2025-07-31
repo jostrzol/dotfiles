939fe438c575a60539ef573f79c6ab464658b29a
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-07-31 13:25:21 +0200

    nvim: lazygit file history

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -7,22 +7,33 @@ return {
       ---@param opts AstroCoreOpts
       opts = function(_, opts)
         local astro = require "astrocore"
+
         -- stolen from autocore/plugins/toggleterm.lua
-        local lazygit = {
-          callback = function()
-            local worktree = astro.file_worktree()
-            local flags = worktree and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir) or ""
-            astro.toggle_term_cmd { cmd = "lazygit " .. flags, direction = "tab" }
-          end,
-          desc = "ToggleTerm lazygit",
-        }
+        ---@param extra_flags string?
+        local callback = function(extra_flags)
+          local worktree = astro.file_worktree()
+          local worktree_flags = worktree
+              and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir)
+            or ""
+          local flags = worktree_flags .. " " .. (extra_flags or "")
+
+          astro.toggle_term_cmd { cmd = "lazygit " .. flags, direction = "tab" }
+        end
 
         ---@type AstroCoreOpts
         local opts_local = {
           mappings = {
             n = {
-              ["<Leader>gg"] = { lazygit.callback, desc = lazygit.desc },
-              ["<Leader>tl"] = { lazygit.callback, desc = lazygit.desc },
+              ["<Leader>gg"] = { callback, desc = "LazyGit" },
+              ["<Leader>tl"] = { callback, desc = "LazyGit" },
+
+              ["<Leader>gf"] = {
+                function()
+                  local path = vim.fn.expand "%:p:~:.:h"
+                  callback("--filter " .. path)
+                end,
+                desc = "LazyGit (file)",
+              },
             },
           },
         }
