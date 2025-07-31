05ff0586a338b483c3810223ba3f67bae60983fd
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-07-31 14:23:20 +0200

    nvim: neotest improve

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -17,26 +17,10 @@ return {
           justMyCode = false,
           console = "integratedTerminal",
         },
-        -- args = {},
-        runner = "unittest",
-        python = { "venv/bin/python3" },
-        is_test_file = function(file_path)
-          if not vim.endswith(file_path, ".py") then return false end
-          local elems = vim.split(file_path, Path.path.sep)
-          local file_name = elems[#elems]
-          return vim.startswith(file_name, "test") or vim.endswith(file_name, "test.py")
-        end,
-        -- python = { "venv/bin/python" },
+        -- args = { "--log-level", "DEBUG", "--quiet" },
+        runner = "pytest",
+        python = { ".venv/bin/python", "venv/bin/python", "/usr/bin/python3" },
       },
-      -- require "neotest-python" {
-      --   dap = {
-      --     justMyCode = false,
-      --     console = "integratedTerminal",
-      --   },
-      --   args = { "--log-level", "DEBUG", "--quiet" },
-      --   runner = "pytest",
-      --   python = { ".venv/bin/python", "/usr/bin/python3" },
-      -- },
       -- require "neotest-plenary",
       -- require "neotest-vim-test" {
       --   ignore_file_types = { "python", "vim", "lua" },
