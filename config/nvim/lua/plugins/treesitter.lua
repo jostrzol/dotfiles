5ff1fdaf28361d4b548bda3fee1aecce6dc287e2
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-09-17 18:20:58 +0200

    nvim: nvim-treesitter.lua -> treesitter.lua

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -11,5 +9,34 @@ return {
       "vim",
       -- add more arguments for adding more treesitter parsers
     },
+
+    -- Disable error messages
+    ignore_install = {},
+    parser_install_dir = nil,
+    auto_install = false,
+    sync_install = false,
+    modules = {},
+
+    -- Extensions
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        node_incremental = "v",
+        node_decremental = "<M-v>",
+      },
+    },
+    textobjects = {
+      select = {
+        keymaps = {
+          ["al"] = "@call.outer",
+          ["il"] = "@call.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+        },
+      },
+    },
+  },
+  specs = {
+    { "windwp/nvim-ts-autotag" },
   },
 }
