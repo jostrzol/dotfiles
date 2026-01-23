0af49f617ca858a807f8e20ceaeaded8ee2802a6
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2026-01-23 20:35:44 +0100

    nvim: move dap config to dap.lua

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -3,6 +3,40 @@
 ---@type LazySpec
 return {
   {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "127.0.0.1",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            vim.fn.expand "$MASON/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+            "${port}",
+            "127.0.0.1",
+          },
+        },
+      }
+      require("dap").adapters["pwa-chrome"] = {
+        type = "server",
+        host = "127.0.0.1",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            vim.fn.expand "$MASON/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+            "${port}",
+            "127.0.0.1",
+          },
+        },
+      }
+
+      -- require('dap').set_log_level('TRACE')
+    end,
+  },
+  {
     "igorlfs/nvim-dap-view",
     ---@module 'dap-view'
     ---@type dapview.Config
